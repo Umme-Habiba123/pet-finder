09d4aa84213dcd3fd0d531e1d3c078f4fad7a71e
Fahim2023 <fahimshahrier2023@gmail.com>
2026-02-01 16:46:03 +0600

    added some comments

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,9 @@ import NavBar from "@/shared/Navbar/Navbar";
 
 export default function Home() {
   return (
-   <div>
-    <NavBar></NavBar>
-   </div>
+    <div>
+      {/* navbar */}
+      <NavBar></NavBar>
+    </div>
   );
 }
